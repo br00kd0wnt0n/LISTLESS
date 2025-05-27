db232eb3ebe1d5b9c9cab3cfafdd28ad490eb9e5
br00kd0wnt0n <brookdownton@me.com>
2025-05-27 19:05:26 -0400

    Simplify Next.js build process

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-  experimental: {
-    outputFileTracingRoot: undefined,
   },
   async headers() {
     return [
